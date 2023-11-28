71654ac86a6c18231ac1743066f7f7273c32dcf7
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-28 16:41:15 +0000

    Added a function that returns the of sums two integers

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def add(a: int, b: int) -> int:
+    """
+    Returns the sum of two integers
+
+    Args:
+        a (int): operand 1
+        b (int): operand 2
+
+    Returns:
+        int: the sum of the two integers
+    """
+    return a + b
