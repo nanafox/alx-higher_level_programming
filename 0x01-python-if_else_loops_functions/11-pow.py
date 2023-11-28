cd4f064e0e0e1f8c0e60426df1388022e46cdac5
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-28 16:53:15 +0000

    Added a function to perform exponentiation

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def pow(a: float, b: float) -> float:
+    """
+    Computes 'a' to the power of 'b' and return the value
+
+    Args:
+        a (float): base
+        b (float): exponent
+
+    Returns:
+        float: the result of 'a' raised to the power of 'b'
+    """
+    return a**b
