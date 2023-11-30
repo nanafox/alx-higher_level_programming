d518542867f3ee1b8398d369dd48901004c8f977
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-30 12:06:20 +0000

    Added code logic to perform basic math operations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+
+def perform_calculation(a: int, b: int) -> None:
+    """
+    Performs simple math operations
+
+    Args:
+        x (int): operand 1
+        y (int): operand 2
+    """
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+
+    for operator, calculator in operators.items():
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, calculator(a, b)))
+
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    # let's perform some basic math operations
+    perform_calculation(a, b)
