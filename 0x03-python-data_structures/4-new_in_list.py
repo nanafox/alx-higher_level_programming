711ed6afa3e3cf5854ee681be81ead4f955cf5a0
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-05 08:40:50 +0000

    Added a to replace an element in a list's copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    """
+    replaces an element in a list at a specific position without modifying
+    the original list (like in C).
+
+    Args:
+        my_list (list): the list containing the data
+        idx (int): the index position
+        element (Any): the replacement value
+
+    Returns:
+        list: a duplicate of the original list with the value updated to the
+        new element's value
+    """
+    dup_list = my_list.copy()
+
+    if 0 > idx >= len(dup_list.copy()):
+        return dup_list
+
+    for i, data in enumerate(dup_list):
+        if i == idx:
+            dup_list[i] = element
+            return dup_list
