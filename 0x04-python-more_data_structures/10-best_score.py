f0e9cc62b47133641e1d449064966bad0f8debd7
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-08 18:02:03 +0000

    Fixed bug when dictionary is empty

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -14,6 +14,8 @@ def best_score(a_dictionary: dict) -> str:
     return (
         # sort based on the number field
         sorted(a_dictionary.items(), key=lambda *items: items[0][1])[-1][0]
-        if a_dictionary is not None
+        # let's ensure the dictionary is not empty and contains some data
+        if a_dictionary is not None and len(a_dictionary) >= 1
+        # well, there was nothing here so do nothing
         else None
     )
