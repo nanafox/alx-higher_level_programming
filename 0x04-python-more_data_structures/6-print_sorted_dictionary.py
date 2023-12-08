8cfe8d6f3472396f27a1f1ea5c6618e1285e87c2
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-08 16:40:59 +0000

    Added a function that prints a dictionrary by ordered keys

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary: dict) -> None:
+    """
+    Prints a dictionary by ordered keys
+
+    Args:
+        a_dictionary (dict): the dictionary to print
+    """
+    for key, value in sorted(a_dictionary.items()):
+        print(f"{key}: {value}")
