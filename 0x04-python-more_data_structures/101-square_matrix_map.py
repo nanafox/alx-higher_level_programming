62225cab66943e2be257ce4d73346d616f5b0d3a
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-09 00:38:58 +0000

    Refactored to match task's requirement

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,29 +1,3 @@
 #!/usr/bin/python3
-
-
-def square_matrix_map(matrix: list = []) -> list[list]:
-    """
-    computes the square value of all integers of a matrix using map
-
-    Args:
-        matrix (list, optional): the matrix. Defaults to [].
-
-    Returns:
-        list[list]: a new matrix with all values squared
-    """
-    if matrix is None:
-        return [[]]
-
-    def operate_on_row(row: list) -> list:
-        """
-        Returns the square for each row in the matrix
-
-        Args:
-            row (list): the row in the matrix
-
-        Returns:
-            list: the current row with the values squared
-        """
-        return list(map(lambda x: x**2, row))
-
-    return list(map(operate_on_row, matrix))
+def square_matrix_map(matrix=[]):
+    return list(map((lambda row: list(map((lambda x: x**2), row))), matrix))
