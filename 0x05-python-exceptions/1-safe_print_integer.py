c090933e8c127daf0fd8b27480f2ead3992dad5e
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-18 12:10:03 +0000

    Added a function to safely print integers

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value) -> bool:
+    """
+    Safely prints an integer value
+
+    Args:
+        value (Any): the value to print
+
+    Returns:
+        bool: True if 'value' is an integer, else False
+    """
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return False  # the value was not an integer
+
+    # the value was indeed an integer and was printed safely
+    return True
