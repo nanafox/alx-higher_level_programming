230480dfc655bb7752a28a2a9a72e80245d575ff
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-18 12:17:40 +0000

    Updated comments for return value on exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -37,5 +37,5 @@ def safe_print_list(my_list=[], x=0) -> int:
 
         return my_list[:x].__len__()
 
-    # this gets printed when an exception occur
+    # return this when an exception occur
     return 0
