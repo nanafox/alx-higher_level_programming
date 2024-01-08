843ac1219f78da9d9f52ec4c3e1d2b490ecffc35
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-01-08 13:44:03 +0000

    Added a method to validate integer values

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+"""A module with a class that models geometric shapes."""
+
+
+class BaseGeometry:
+    """A base class for geometry shapes."""
+
+    def area(self):
+        """Returns the area of a shape"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name: str, value: int) -> None:
+        """
+        Performs validation of integer values.
+
+        Args:
+            name (str): The name of the object.
+            value (int): The value of the object.
+
+        Raises:
+            TypeError: When the `value` given to the object is not an integer.
+            ValueError: When the the `value` is less than or equal to zero.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
