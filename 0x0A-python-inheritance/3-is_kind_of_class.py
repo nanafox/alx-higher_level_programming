35a6bb6528cf4caa6d37652fddfe8d81c06bc7f1
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-01-08 13:14:40 +0000

    Added a function checks for types based on inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+A module with a function that returns True if the object is an
+instance of the specified class.
+"""
+
+
+def is_kind_of_class(obj: object, a_class) -> bool:
+    """
+    Returns True if `obj` is an instance of `a_class`.
+
+    Args:
+        obj (object): The object to check.
+        a_class (class): The class to check if `obj` is an instance of.
+
+    Returns:
+        bool: True is `obj` is an instance of `a_class`, False otherwise.
+    """
+    return isinstance(obj, a_class)
