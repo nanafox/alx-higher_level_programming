095c28f594a4d3ca4105fe45bb2b40d99ee2bba2
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-01-09 15:31:53 +0000

    Added a function that creates Python objects from JSON files

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""A module with a functiont that creates an Object from a JSON file."""
+
+import json
+
+
+def load_from_json_file(filename: str) -> object:
+    """
+    Creates a Python object from a JSON file.
+
+    Args:
+        filename (str): The file containing the object's JSON reprensentation.
+
+    Returns:
+        object: The associated Python object  represented in the JSON file.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
+
