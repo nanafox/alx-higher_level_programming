81aa53f972f1f3824ff37ebcb3d2b50b28cf6412
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-01-09 21:34:03 +0000

    Improved the 'to_json()' method to retrieve specific attributes

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""A module that models a Student object."""
+
+
+class Student:
+    """Models a Student object."""
+
+    def __init__(self, first_name: str, last_name: str, age: int) -> None:
+        """
+        Initializes / creates a new Student object.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None) -> dict:
+        """
+        Returns the dictionary representation of the `Student` object.
+
+        Args:
+            attr: A list of attribute names to retrieve.
+
+        Returns:
+            dict: The dictionary representation of the `Student` object.
+        """
+        if attr is not None and isinstance(attr, list):
+            return (
+                {
+                    key: value for key, value in self.__dict__.items()
+                    if key in attr
+                }
+            )
+
+        # no specific attributes were given, return everything
+        return self.__dict__
