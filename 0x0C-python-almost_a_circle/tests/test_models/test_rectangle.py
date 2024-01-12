38a863e07c0144d3abf211698352e2124d707bf4
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-01-12 12:44:23 +0000

    Added more test cases for Rectangle class: v1.2

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,6 +2,7 @@
 
 """Tests the rectangle module."""
 
+from ctypes.wintypes import HENHMETAFILE
 from models.rectangle import Rectangle
 import unittest
 
@@ -22,6 +23,43 @@ class TestRectangle(unittest.TestCase):
 
         # reset y values
         self.r1.y, self.r2.y, self.r3.y = 0, 0, 0
+
+    # Test for missing positional arguments
+
+    def test_missing_positional_width_arg(self):
+        """Tests for the missing width attribute when instantiating."""
+        with self.assertRaises(TypeError) as no_width:
+            Rectangle(height=2)
+
+        self.assertEqual(
+            no_width.exception.__str__(),
+            "__init__() missing 1 required positional argument: 'width'",
+        )
+
+    def test_missing_positional_height_arg(self):
+        """Tests for the missing height attribute when instantiating."""
+        with self.assertRaises(TypeError) as no_height:
+            Rectangle(width=10)
+
+        self.assertEqual(
+            no_height.exception.__str__(),
+            "__init__() missing 1 required positional argument: 'height'",
+        )
+
+    def test_missing_positional_args(self):
+        """
+        Tests for the missing width and height attributes when instantiating.
+        """
+        with self.assertRaises(TypeError) as no_height_width:
+            Rectangle()
+
+        self.assertEqual(
+            no_height_width.exception.__str__(),
+            "__init__() missing 2 required positional arguments: "
+            "'width' and 'height'",
+        )
+
+    # Test for expected behavior when the attributes are correctly set.
 
     def test_width_values(self):
         """Tests the width values of instantiated objects."""
@@ -71,6 +109,8 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(self.r2.y, 10)
         self.assertEqual(self.r3.y, 15)
 
+    # Test to ensure no two objects have the same ID
+
     def test_set_ids(self):
         """Tests objects that sets their own IDs."""
         self.assertEqual(self.r3.id, 12)
@@ -80,3 +120,115 @@ class TestRectangle(unittest.TestCase):
         self.assertNotEqual(self.r1.id, self.r2.id)
         self.assertNotEqual(self.r3.id, self.r2.id)
         self.assertNotEqual(self.r1.id, self.r3.id)
+
+    # Test for TypeError exceptions raised when setting the attributes.
+
+    def test_invalid_type_for_width_set(self):
+        """
+        Tests for TypeError exceptions when setting or updating the `width`
+        attribute.
+        """
+        with self.assertRaises(TypeError) as type_error:
+            self.r1.width = 1.4
+            self.r1.width = True
+            self.r4.width = "five"
+
+        # ensure the exception message is as expected
+        self.assertEqual(
+            type_error.exception.__str__(), "width must be an integer"
+        )
+
+    def test_invalid_type_for_height_set(self):
+        """
+        Tests for TypeError exceptions when setting or updating the `height`
+        attribute.
+        """
+        with self.assertRaises(TypeError) as type_error:
+            self.r1.height = 1.4
+            self.r1.height = True
+            self.r4.height = "five"
+
+        # ensure the exception message is as expected
+        self.assertEqual(
+            type_error.exception.__str__(), "height must be an integer"
+        )
+
+    def test_invalid_type_for_x_set(self):
+        """
+        Tests for TypeError exceptions when setting or updating the `x`
+        attribute.
+        """
+        with self.assertRaises(TypeError) as type_err:
+            self.r1.x = 1.4
+            self.r1.x = True
+            self.r4.x = "five"
+
+        # ensure the exception message is as expected
+        self.assertEqual(type_err.exception.__str__(), "x must be an integer")
+
+    def test_invalid_type_for_y_set(self):
+        """
+        Tests for TypeError exceptions when setting or updating the `y`
+        attribute.
+        """
+        with self.assertRaises(TypeError) as type_err:
+            self.r1.y = 1.4
+            self.r1.y = True
+            self.r4.y = "five"
+
+        # ensure the exception message is as expected
+        self.assertEqual(type_err.exception.__str__(), "y must be an integer")
+
+    # Test for ValueError exceptions raised when setting the attributes.
+
+    def test_invalid_value_for_width_set(self):
+        """
+        Tests for TypeError exceptions when setting or updating the `width`
+        attribute.
+        """
+        with self.assertRaises(ValueError) as value_error:
+            self.r1.width = -34
+            self.r1.width = -1
+            self.r4.width = 0
+
+        # ensure the exception message is as expected
+        self.assertEqual(value_error.exception.__str__(), "width must be > 0")
+
+    def test_invalid_value_for_height_set(self):
+        """
+        Tests for ValueError exceptions when setting or updating the `height`
+        attribute.
+        """
+        with self.assertRaises(ValueError) as value_error:
+            self.r1.height = -34
+            self.r1.height = -1
+            self.r4.height = 0
+
+        # ensure the exception message is as expected
+        self.assertEqual(value_error.exception.__str__(), "height must be > 0")
+
+    def test_invalid_value_for_x_set(self):
+        """
+        Tests for ValueError exceptions when setting or updating the `x`
+        attribute.
+        """
+        with self.assertRaises(ValueError) as value_error:
+            self.r1.x = 0
+            self.r1.x = -1
+            self.r4.x = -45
+
+        # ensure the exception message is as expected
+        self.assertEqual(value_error.exception.__str__(), "x must be >= 0")
+
+    def test_invalid_value_for_y_set(self):
+        """
+        Tests for ValueError exceptions when setting or updating the `y`
+        attribute.
+        """
+        with self.assertRaises(ValueError) as value_error:
+            self.r1.y = 0
+            self.r1.y = -1
+            self.r4.y = -45
+
+        # ensure the exception message is as expected
+        self.assertEqual(value_error.exception.__str__(), "y must be >= 0")
