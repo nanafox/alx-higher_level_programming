589c3b21c1cb2675fbfb7f877de4cba966d893db
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-01-13 21:28:34 +0000

    Added a function that returns the JSON string representation of a list of dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Defines the base class class for future classes."""
 
+import json
+
 
 class Base:
     """Base object for future classes."""
@@ -19,3 +21,29 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries: "list[dict]") -> str:
+        """
+        Returns the JSON string representation of a list of dictionaries.
+
+        Args:
+            list_dictionaries (list[dict]): The list of dictionaries.
+
+        Returns:
+            str: The JSON string representation of objects in the
+            `list_dictionaries` else "[]" if empty.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        if type(list_dictionaries) is not list:
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+
+        for obj in list_dictionaries:
+            if type(obj) is not dict:
+                raise TypeError(
+                    "list_dictionaries must be a list of dictionaries"
+                )
+
+        return json.dumps(list_dictionaries)
