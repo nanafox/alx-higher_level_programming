e7faf9213e2459ab43b303f52e5f68c0c9981701
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-01-14 12:27:12 +0000

    Added a function that saves object representation to JSON
    While here, I added some extra documentation to a previous function.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,6 +30,10 @@ class Base:
         Args:
             list_dictionaries (list[dict]): The list of dictionaries.
 
+        Raises:
+            TypeError: If the argument received `list_dictionaries` is not a
+            list or does not contain a list of dictionaries.
+
         Returns:
             str: The JSON string representation of objects in the
             `list_dictionaries` else "[]" if empty.
@@ -47,3 +51,38 @@ class Base:
                 )
 
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs: list) -> None:
+        """
+        Writes the JSON string representation of a list of objects to a file.
+
+        `list_objs` is a list of instances who inherits from the `Base` class.
+        This function uses the concept of polymorphism so it allows for a list
+        of mixed objects that inherits from `Base`.
+
+        Args:
+            list_objs (list): The list of objects that inherits from `Base`.
+
+        Raises:
+            TypeError: If any of the objects in `list_objs` is not an instance
+            of the `Base` class.
+        """
+        dict_of_objs = {}
+
+        for obj in list_objs:
+            if not isinstance(obj, Base):
+                raise TypeError(
+                    "list_objs must be a list of objects who inherit from Base"
+                )
+
+            class_name = obj.__class__.__name__
+            if class_name not in dict_of_objs:
+                dict_of_objs[class_name] = []
+
+            dict_of_objs[class_name].append(obj.to_dictionary())
+
+        for class_name, list_dictionaries in dict_of_objs.items():
+            json_dictionary = cls.to_json_string(list_dictionaries)
+            with open(f"{class_name}.json", "w", encoding="utf-8") as file:
+                json.dump(json.loads(json_dictionary), file)
