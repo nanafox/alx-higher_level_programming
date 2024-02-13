094e79cc36d3ec5465c07eb0e155b65f6807292e
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-02-13 19:59:20 +0000

    Added a script that computes the average of scores

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- This script computes the the score average of all records in the
+-- table 'second_table' of the database 'hbtn_0c_c'
+
+-- compute the average of the scores
+SELECT AVG(score) AS average
+FROM second_table;
