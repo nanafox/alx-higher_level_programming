9b88234b099eba001c61f75795fec2ef91f70050
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-02-13 20:15:54 +0000

    Added a script that lists the frequency of scores

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,9 @@
+-- This script lists the number of records with the same score in the table
+-- 'second_table' of the database 'hbtn_0c_0'
+
+-- list the scores and the number of times they occur
+SELECT score, count(score) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number
+DESC;
