9e7b74ce0893977abd7bdb067ea49775a5cf8ef3
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-02-13 14:40:18 +0000

    Added a script that creates the 'first_table' table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- This script creates a table called 'first_table' in the current database
+
+-- define and create the table if it does not exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
