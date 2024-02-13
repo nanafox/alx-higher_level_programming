f964bd3c00373c9808280236061a8b06c884854a
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-02-13 16:14:54 +0000

    Added a script that counts 'id' with value '89'

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- This script displays the number of records with `id = 89` in the
+-- 'first_table' of the database 'hbtn_0c_0'
+
+-- display the number of records whose `id` is equal to `89`
+SELECT COUNT(id) FROM first_table WHERE id = 89
