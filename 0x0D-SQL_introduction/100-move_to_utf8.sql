f74b40aec30d72953c86d85ce578bbcf39cdbb4e
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-02-14 06:58:50 +0000

    Added the SQL database to use

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,8 +3,8 @@
 -- alter the database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- select the 'hbtn_0c_0' database
+USE hbtn_0c_0;
+
 -- alter the table 'first_table'
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- alter the 'name' field in the 'first_table'
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
