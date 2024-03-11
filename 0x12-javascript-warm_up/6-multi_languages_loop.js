42de2df2fee3391c5d4c115cab005f8c4903c183
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-11 14:21:39 +0000

    Add a script uses a loop to print messages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Prints a message about languages in an array using a loop.
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+languages.forEach(language => console.log(language));
