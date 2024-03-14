b64b85fe5cfb4b94c60ca07646feb077c2e4a88f
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-14 17:42:28 +0000

    Add a script that filters by a state: SQL-injectable

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""This script takes in an argument and displays all values in the `states`
+table of the database `hbtn_0e_0_usa` where the `name` matches the argument."""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    username, password, db_name, state_name = sys.argv[1:]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        password=password,
+        database=db_name,
+        port=3306,
+    )
+
+    cursor = conn.cursor()
+
+    # The query is written in a way that is unsafe from SQL injection
+    query = """
+        SELECT * FROM states
+        WHERE name = '{}'
+        ORDER BY states.id;
+    """.format(state_name)
+
+    cursor.execute(query)
+
+    result_set = cursor.fetchall()
+
+    for row in result_set:
+        print(row)
+
+    cursor.close()
+    conn.close()
