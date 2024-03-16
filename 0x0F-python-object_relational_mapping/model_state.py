b8d3cede5cf9cb80da3d258d9f070dc9452184b9
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-16 10:57:13 +0000

    Add a class to define the States object

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""This module defines the States class."""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Defines the State class."""
+
+    __tablename__ = "states"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True,
+        unique=True,
+    )
+
+    name = Column(String(128), nullable=False)
