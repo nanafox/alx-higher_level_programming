a7a21b0f9e651074172deb288835c095ea71dd90
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-17 10:51:04 +0000

    Add a script list State objects and corresponding City objects

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""This script lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from relationship_city import City
+from relationship_state import State, Base
+
+if __name__ == "__main__":
+    try:
+        username, password, database = sys.argv[1:]
+    except ValueError:
+        sys.stderr.write(
+            f"Usage: {sys.argv[0]} <username> <password> <database>\n"
+        )
+        sys.exit(1)
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
+        pool_pre_ping=True,
+    )
+
+    Base.metadata.create_all(engine)
+    with Session(engine) as session:
+        query = session.query(State).order_by(State.id)
+        for state in query:
+            print(f"{state.id}: {state.name}")
+            for city in state.cities:
+                print(f"    {city.id}: {city.name}")
