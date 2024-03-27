b25acb5dd6d393b6f681a907199d422312e3af04
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-27 15:39:22 +0000

    Change from 'http_code' to newer 'response_code'

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -so /dev/null -w "%{http_code}" "$1"
+curl -so /dev/null -w "%{response_code}" "$1"
