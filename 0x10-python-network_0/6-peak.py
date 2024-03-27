a275e6c88aeeaa800df43ad93bd12f4f2ab46602
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-27 22:07:29 +0000

    Add a Python script that finds the peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers) -> int | None:
+    """This function finds a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): A list of unsorted integers.
+
+    Returns:
+        int | None: The peak of the list of integers.
+    """
+    if not list_of_integers:
+        return None
+
+    list_of_integers.sort()
+    return list_of_integers[-1]
