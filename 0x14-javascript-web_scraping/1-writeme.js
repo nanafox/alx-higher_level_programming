e7aae9a16aa976fc109204f2acd46bdf2c4ed18d
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-23 18:23:20 +0000

    Add a script writes data to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+// This script writes the contents received on the CLI to a file.
+
+const fs = require('fs');
+const path = require('path');
+
+function writeToFile () {
+  if (process.argv.length !== 4) {
+    console.error(`Usage: ${path.basename(process.argv[1])} <filename> <data>`);
+    return;
+  }
+
+  const filename = process.argv[2];
+  const data = process.argv[3];
+  fs.writeFile(filename, data, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+}
+
+writeToFile();
