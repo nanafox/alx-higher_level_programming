8ce0ffadd11a7ae419297466ae4544eb42449b93
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-23 21:55:33 +0000

    Refactor code so it prints output in order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
 // This script prints all characters of a Star Wars movie
+// Character names are displayed in the same order of the list “characters”
+// in the films response
 
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
@@ -8,16 +10,27 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, (error, response) => {
   if (error) {
     console.error(error);
-  } else {
-    const characters = JSON.parse(response.body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response) {
+    return;
+  }
+
+  const characters = JSON.parse(response.body).characters;
+  const characterPromises = characters.map(character => {
+    return new Promise((resolve, reject) => {
+      request(character, (error, response) => {
         if (error) {
-          console.error(error);
+          reject(error);
         } else {
-          console.log(JSON.parse(response.body).name);
+          resolve(JSON.parse(response.body).name);
         }
       });
     });
-  }
+  });
+
+  Promise.all(characterPromises)
+    .then(names => {
+      names.forEach(name => console.log(name));
+    })
+    .catch(error => {
+      console.error(error);
+    });
 });
