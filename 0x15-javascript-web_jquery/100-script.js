e70aa42d7dc4fbc6ac09d0141888ab42d18856d1
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-05-02 23:41:07 +0000

    feat: Update text color of header element to red
    This commit adds code to update the text color of the `<header>` element to red (#FF0000) using JavaScript. The change is triggered when the DOM content is loaded. This enhancement improves the visual appearance of the webpage.

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// updates the text color of the <header> element to red (#FF0000):
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
