4b3148849491b9a3ce05092f7b716acb970a3911
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-05-03 00:14:27 +0000

    feat: Fetch and display hello value from API in div#hello

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,12 @@
+// fetches and prints how to say “Hello” depending on the language
+
+const url = 'https://www.fourtonfish.com/hellosalut/?lang=';
+
+$(document).ready(() => {
+  $('input#btn_translate').on('click', () => {
+    const lang = $('input#language_code').val();
+    $.get(url + lang, (data) => {
+      $('div#hello').text(data.hello);
+    });
+  });
+});
